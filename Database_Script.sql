63e361626ccf7981074f2b4ecc1b1d79d74c77f6
Mark Hojnacki <odin@ODIN.localdomain>
2019-11-20 15:15:19 -0500

    Just creating files

---
+++ Database_Script.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE Final408Project;
+USE Final408Project;
+
+CREATE TABLE Test_Table (
+	Id INT auto_increment primary key,
+    Title VARCHAR(50) NOT NULL,
+    Artist VARCHAR(100) NOT NULL,
+    Country VARCHAR(100) NOT NULL,
+    Company VARCHAR(100) NOT NULL,
+    Price FLOAT NOT NULL,
+    Year INT NOT NULL
+);
+
+Select * from Test_Table;
